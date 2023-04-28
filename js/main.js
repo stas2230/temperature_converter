a69561c156fc2e0e53bd527e6a3424bdc72f0e59
Michał Pastuszak <mpastuszak5@gmail.com>
2023-04-28 15:35:43 +0200

    convertCelsToFahr and convertFahrToCels funstions creates

--- js/main.js
+++ js/main.js
@@ -1,22 +1,59 @@
 
-const result = document.querySelector(".result")
-const termeratureUnit1 = document.querySelector(".termerature_unit1")
-const termeratureUnit2 = document.querySelector(".termerature_unit2")
+const result = document.querySelector(".converter__result")
+const temperatureUnit1 = document.querySelector(".temperature_unit1")
+const temperatureUnit2 = document.querySelector(".temperature_unit2")
 const input = document.querySelector("#converter")
 const btnConvert = document.querySelector(".conventer__btn--convert")
 const btnSwap = document.querySelector(".conventer__btn--swap")
 const btnReset = document.querySelector(".conventer__btn--reset")
 
+let cels 
+let fahr 
+
+
 const swapTemperatureUnit = () => {
 
-    if (termeratureUnit1.textContent === '°C'){
-        termeratureUnit1.textContent = '°F'
-        termeratureUnit2.textContent = '°C'
+    if (temperatureUnit1.textContent === '°C'){
+        temperatureUnit1.textContent = '°F'
+        temperatureUnit2.textContent = '°C'
     } else {
-        termeratureUnit1.textContent = '°C'
-        termeratureUnit2.textContent = '°F'
+        temperatureUnit1.textContent = '°C'
+        temperatureUnit2.textContent = '°F'
     }
 
 }
 
+const convertCelsToFahr = () => {
+    
+    fahr = input.value * 1.8 + 32
+    result.textContent = `${input.value}°C is equatl to ${fahr.toFixed(1)}°F`
+    input.value = ''
+}
+
+const convertFahrToCels = () => {
+
+    cels = (input.value - 32) / 1.8 
+    result.textContent = `${input.value}°F is equatl to ${cels.toFixed(1)}°C`
+    input.value = ''
+}
+
+
+
 btnSwap.addEventListener('click', swapTemperatureUnit);
+btnConvert.addEventListener('click', convertFahrToCels)
+
+
+// notes:
+
+// convertCelcToFahr function:
+// Temperature in degrees Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32.
+// adding relevant text to element with .converter__result 
+// clean input value after conversion
+
+// convertFahrToCels function:
+// Temperature in degrees Celsius (°C) = (Temperature in degrees Fahrenheit (°F) - 32) / 1.8.
+// adding relevant text to element with .converter__result
+// clean input value after conversion
+
+// give the result rounded to one decimal place 
+
